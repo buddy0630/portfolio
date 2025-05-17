7c84fdbf4d690766bbbc57736c874bbbff1b9ac5
buddy0630 <121750244+buddy0630@users.noreply.github.com>
2025-05-18 03:06:29 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import NavBar from "./components/Navbar"; 
+import NavBar from "./components/NavBar"; 
 
 import About from './components/About';
 import Projects from './components/Projects';
